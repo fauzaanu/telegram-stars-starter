032f6960a5b229d3a76819366615fe5b47d79092
fauzaanu <fauzaangasim@gmail.com>
2024-06-13 03:59:11 +0500

    feat: examples for refunds, successful star payments added

--- bot.py
+++ bot.py
@@ -47,6 +47,32 @@ async def precheckout_callback(update, context):
         await query.answer(ok=True)
 
 
+async def successful_payment_callback(update, context):
+    """
+    This is recieved on successful payment.
+    chargeback id can be found by clicking the service message in the chat. (if you are testing).
+    This is a good place to store the charg id in your database and to process your internal balance
+    or subscription logic.
+    """
+    print(update.message.successful_payment)
+
+
+async def refund_payment(update, context):
+    """
+    This is a sample refund function.
+    """
+    db = "LETS SAY THIS IS YOUR DB AND YOU HAVE PAYMENTS STORED HERE"  # just an example, dont be insane
+    status = await context.bot.refund_star_payment(
+        user_id=update.message.chat.id,
+        telegram_payment_charge_id=db.telegram_charge_id
+    )
+    if status:
+        await context.bot.send_message(
+            chat_id=update.message.chat.id,
+            text=f'Your payment {db.telegram_charge_id} has been refunded successfully.'
+        )
+
+
 if __name__ == '__main__':
     load_dotenv()
     token = os.environ['TELEGRAM_BOT_TOKEN']
@@ -54,6 +80,8 @@ if __name__ == '__main__':
 
     commands = CommandHandler('start', start_command)
     invoice = CommandHandler('invoice', send_invoice)
+    refund = CommandHandler('refund', refund_payment)
+    successful_payment = MessageHandler(filters.SUCCESSFUL_PAYMENT, successful_payment_callback)
     links = MessageHandler(filters.TEXT, hi_hello)
 
     application.add_handler(commands)
