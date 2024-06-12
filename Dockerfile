ee8a79d4a76901901954365a77cd5e3f1be4d182
fauzaanu <fauzaangasim@gmail.com>
2024-06-12 05:34:25 +0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.12-slim-bookworm
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /app
+WORKDIR /app
+RUN pip install poetry && poetry config virtualenvs.create false
+COPY . /app/
+RUN ls /app/
+RUN poetry install --no-interaction --no-root
---
+++ bot.py
@@ -0,0 +1,69 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import LabeledPrice
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, PreCheckoutQueryHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start_command(update, context):
+    await context.bot.send_message(
+        chat_id=update.message.chat.id,
+        text='Welcome to the bot! How can I help you?'
+    )
+
+
+async def hi_hello(update, context):
+    await context.bot.send_message(
+        chat_id=update.message.chat.id,
+        text='Hi, Hello! How is life?'
+    )
+
+
+async def send_invoice(update, context):
+    await context.bot.send_invoice(
+        chat_id=update.message.chat.id,
+        title='Sample Invoice',
+        description='This is a sample invoice',
+        payload='WPBOT-PYLD',
+        currency='XTR',
+        prices=[
+            LabeledPrice('Basic', 100),
+            LabeledPrice('Premium', 200),
+            LabeledPrice('Pro', 300),
+        ],
+        provider_token='',
+    )
+
+
+async def precheckout_callback(update, context):
+    query = update.pre_checkout_query
+    if query.invoice_payload != 'WPBOT-PYLD':
+        await query.answer(ok=False, error_message="Something went wrong...")
+    else:
+        await query.answer(ok=True)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.getenv('TELEGRAM_BOT_TOKEN')
+    application = ApplicationBuilder().token(token).build()
+
+    commands = CommandHandler('start', start_command)
+    invoice = CommandHandler('invoice', send_invoice)
+    links = MessageHandler(filters.TEXT, hi_hello)
+
+    application.add_handler(commands)
+    application.add_handler(invoice)
+
+    # Pre-checkout handler to final check
+    application.add_handler(PreCheckoutQueryHandler(precheckout_callback))
+
+    application.add_handler(links)
+
+    application.run_polling()
